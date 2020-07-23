650c86fe90888e8bb9c618414c0aba7c25a28850
Amayo II <amayomordecai@gmail.com>
2020-07-23 23:25:54 +0300

    Updated train_model

--- train_model.py
+++ train_model.py
@@ -1,5 +1,5 @@
 # USAGE
-# python train_mask_detector.py --dataset dataset
+# python train_model.py --dataset dataset
 
 # import the necessary packages
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
